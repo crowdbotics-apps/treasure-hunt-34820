5b0a05237fedfd2071f2466c06e503be49913042
Crowdbotics <team@crowdbotics.com>
2022-04-19 04:04:12 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,19 @@
+from django.conf import settings
 from django.db import models
+
+
+class Location(models.Model):
+    "Generated Model"
+    streetName = models.CharField(
+        max_length=256,
+    )
+    city = models.CharField(
+        max_length=256,
+        null=True,
+        blank=True,
+    )
+    state = models.CharField(
+        max_length=256,
+        null=True,
+        blank=True,
+    )
